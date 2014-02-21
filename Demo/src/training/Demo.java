59b660005394ce79fd0f3de94047355733eb0698
pbhawsar <pbhawsar@paypal.com>
2014-02-21 00:52:56 -0800

    adding few more files

---
+++ Demo/src/training/Demo.java
@@ -0,0 +1,15 @@
+package training;
+
+import java.util.Scanner;
+
+public class Demo {
+
+	public static void main( String[] args) throws Exception {
+
+	    char ch = 'A' ;
+	    System.out.println("A char: " +  ch );
+
+
+	}
+
+}
